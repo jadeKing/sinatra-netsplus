38b9095360bb6809d271aa0d4bd319dcf4fc9120
Jade King <dev@jadeking.co.uk>
2015-04-06 00:36:41 +0100

    tutorial part 1 complete

---
+++ basics.rb
@@ -0,0 +1,43 @@
+require 'sinatra'
+
+get '/' do
+  'Hello, World!'
+end
+
+get '/about' do
+  'A little about me.'
+end
+
+get '/hello/:name/:city' do
+  "Howdy, #{params[:name].upcase} from #{params[:city]}"
+end
+
+get '/more/*' do
+  params[:splat]
+end
+
+get '/form' do
+  erb :form
+end
+
+post '/form' do
+  "You said '#{params[:message]}'"
+end
+
+get '/secret' do
+  erb :secret
+end
+
+post '/secret' do
+  params[:secret].reverse
+end
+
+get '/decrypt/:secret' do
+  params[:secret].reverse
+end
+
+not_found do
+  # status 404
+  # 'not found'
+  halt 404, 'page not found'
+end
